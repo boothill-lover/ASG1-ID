1cd95ab8529c23aebdf53672480e3dcffc7bd201
boothill-lover <S10275365@connect.np.edu.sg>
2025-11-25 22:16:37 +0800

    refined navbar

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// change from hamburger to an X icon
 function myFunction(x) {
             x.classList.toggle("change");
         }
+
+// "pulls out" side nav bar and closes
+function togglenav() {
+    const sidenav = document.getElementById("sidenavbar");
+    const navicon = document.getElementById("navicon");
+
+    // if it is open, close it
+    if (sidenav.style.width == "250px") {
+        sidenav.style.width = "0";
+        navicon.style.right = "60px";
+    } 
+    else {
+        sidenav.style.width = "250px";
+        navicon.style.right = "310px";
+    }
+}
